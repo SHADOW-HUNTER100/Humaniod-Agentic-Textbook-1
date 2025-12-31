9ac338b0b7bf7cd2344c6d07f0e01b4e66634d1a
DEVELOPER <uddinmasahim@gmail.com>
2025-12-31 15:03:34 +0500

    commit

--- Textbook/docs/src/components/Auth/index.tsx
+++ Textbook/docs/src/components/Auth/index.tsx
@@ -1,11 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import clsx from 'clsx';
+import { useLocation } from '@docusaurus/router';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
 import styles from './Auth.module.css';
 
 const AuthPage: React.FC = () => {
-  const [isSignIn, setIsSignIn] = useState(true);
+  const location = useLocation();
+  const [isSignIn, setIsSignIn] = useState(location.pathname.includes('sign-in'));
+
+  // Update the state when the location changes
+  useEffect(() => {
+    setIsSignIn(location.pathname.includes('sign-in'));
+  }, [location.pathname]);
 
   return (
     <div className={styles.authContainer}>
